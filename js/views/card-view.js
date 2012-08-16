10b8073cc05f51041219f57b8517204647226597
Sarah Vessels <cheshire137@gmail.com>
2012-08-16 19:40:44 -0400

    Adding more SVG cards, including kings, queens, and jacks for diamonds and clubs.

--- js/views/card-view.js
+++ js/views/card-view.js
@@ -7,10 +7,11 @@ CardView = Backbone.View.extend({
 		var card = this.options.card;
 		var cardName = card.get('name');
 		var suitName = card.get('suit').get('name');
-		if (cardName == 'king' && suitName == 'diamond') {
-			console.log("SVGCardView for king of diamonds");
-			var svgCardView = new SVGCardView({el: this.el, card: card});
-			return;
+		if (suitName == 'diamond' || suitName == 'club') {
+			if ($.inArray(cardName, ['king', 'jack', 'queen']) != -1) {
+				var svgCardView = new SVGCardView({el: this.el, card: card});
+				return;
+			}
 		}
 		var variables = {card_name: cardName, suit_name: suitName};
 		var template = _.template($('#card-view-template').html(), variables);
