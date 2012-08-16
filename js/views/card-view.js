cb11b5815511789a2938d5a1d8bb2ffd81650916
Sarah Vessels <cheshire137@gmail.com>
2012-08-15 22:29:14 -0400

    Using SVG card images instead of raster images.  Adding TextMate project.  Only have King of Diamonds SVG in place so far.  Highlighting cards on select.  Allowing cards to be moved to the clock face if you try to move to the same suit and increase the number.

--- js/views/card-view.js
+++ js/views/card-view.js
@@ -5,7 +5,14 @@ CardView = Backbone.View.extend({
 	},
 	render: function() {
 		var card = this.options.card;
-		var variables = {card_name: card.get('name'), suit_name: card.get('suit').get('name')};
+		var cardName = card.get('name');
+		var suitName = card.get('suit').get('name');
+		if (cardName == 'king' && suitName == 'diamond') {
+			console.log("SVGCardView for king of diamonds");
+			var svgCardView = new SVGCardView({el: this.el, card: card});
+			return;
+		}
+		var variables = {card_name: cardName, suit_name: suitName};
 		var template = _.template($('#card-view-template').html(), variables);
 		$(this.el).html(template);
 		var suitContainer = $('.card .suit-container', $(this.el));
@@ -16,12 +23,53 @@ CardView = Backbone.View.extend({
 	},
 	onCardClick: function(event) {
 		var cardEl = $(event.currentTarget);
+		var inTableau = cardEl.closest('.tableau').length > 0;
+		if (inTableau) {
+			this.onTableauCardClick(cardEl);
+		} else {
+			this.onClockCardClick(cardEl);
+		}
+	},
+	onTableauCardClick: function(cardEl) {
 		var cardContainer = cardEl.parent();
 		if (!cardContainer.is(':last-child')) {
 			return;
 		}
-		var cardName = cardEl.attr('data-card-name');
-		var suitName = cardEl.attr('data-suit-name');
-		console.debug("Card clicked " + cardName + ", " + suitName);
+		$('.card.selected').removeClass('selected');
+		cardEl.addClass('selected');
+		$('.clock-face').addClass('tableau-card-selected');
+	},
+	onClockCardClick: function(clockCardEl) {
+		var cardToMoveEl = $('.tableau .card.selected');
+		var foundation = clockCardEl.parent();
+		if (this.canMoveCard(cardToMoveEl, clockCardEl)) {
+			this.moveCardToClock(cardToMoveEl, foundation);
+		}
+	},
+	canMoveCard: function(cardToMoveEl, clockCardEl) {
+		var clockCardSuit = clockCardEl.attr('data-suit-name');
+		var cardToMoveSuit = cardToMoveEl.attr('data-suit-name');
+		if (cardToMoveSuit != clockCardSuit) {
+			return false;
+		}
+		var clockCardName = clockCardEl.attr('data-card-name');
+		var cardToMoveName = cardToMoveEl.attr('data-card-name');
+		if (
+			clockCardName == 'ace' && cardToMoveName == '2' ||
+			clockCardName == '10' && cardToMoveName == 'jack' ||
+			clockCardName == 'jack' && cardToMoveName == 'queen' ||
+			clockCardName == 'queen' && cardToMoveName == 'king'
+		) {
+			return true;
+		}
+		var clockCardNum = parseInt(clockCardName, 10);
+		var cardToMoveNum = parseInt(cardToMoveName, 10);
+		return cardToMoveNum - 1 == clockCardNum;
+	},
+	moveCardToClock: function(cardToMoveEl, foundation) {
+		var cardContainer = cardToMoveEl.parent();
+		cardToMoveEl.appendTo(foundation).removeClass('selected');
+		cardContainer.remove();
+		$('.clock-face.tableau-card-selected').removeClass('tableau-card-selected');
 	}
 });
