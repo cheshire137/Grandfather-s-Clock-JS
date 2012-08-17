ab70b35cf9a819c0814ea8799288fb3c8bc3fec3
Sarah Vessels <cheshire137@gmail.com>
2012-08-16 21:53:51 -0400

    Adding options panel that can be expanded with a pull tab.  Adding background option with a choice between wood and green felt.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 (function($) {
 	var startView = new StartView({el: $('#container')});
+	$('#options-pull-tab').toggle(function(event) {
+		event.preventDefault();
+		$('#options').css('margin-top', 0);
+	}, function(event) {
+		event.preventDefault();
+		var options = $('#options');
+		options.css('margin-top', '-' + options.height() + 'px');
+	});
+	$('#background-options a').click(function(event) {
+		var href = $(this).attr('href');
+		var className = href.replace(/^#/, '');
+		$('body').removeClass('felt').removeClass('wood').addClass(className);
+	});
 })(jQuery);
